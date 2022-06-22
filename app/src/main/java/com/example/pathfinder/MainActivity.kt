42e1e92b6f3650e0630b7851b349f1effeac92ab
ireneMiller <iramelnykv@gmail.com>
2022-06-22 10:32:47 +0300

    added new drawables for main activity

--- app/src/main/java/com/example/pathfinder/MainActivity.kt
+++ app/src/main/java/com/example/pathfinder/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.pathfinder
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pathfinder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding.createButton.setOnClickListener {
+
+        }
+
+        //load data from the file
+        binding.loadButton.setOnClickListener {
+
+        }
+
+        //open new fragment with customization options
+        binding.customizeButton.setOnClickListener {
+
+        }
+
+        //manual on how to use the app
+        binding.helpButton.setOnClickListener {
+
+        }
     }
 }
