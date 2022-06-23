408eb646d1e3e6197a5302de7fd45aee78bf7ccd
ireneMiller <iramelnykv@gmail.com>
2022-06-23 09:45:14 +0300

    main activity opens customThemeFragment

--- app/src/main/java/com/example/pathfinder/MainActivity.kt
+++ app/src/main/java/com/example/pathfinder/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.example.pathfinder
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.pathfinder.customization.CustomizationFragment
+import com.example.pathfinder.customization.ThemeManager
+import com.example.pathfinder.customization.ThemeManager.Companion.setCustomTheme
+import com.example.pathfinder.customization.ThemeStorage
 import com.example.pathfinder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setCustomTheme(this, ThemeStorage.getThemeColor(this).toString())
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         binding.createButton.setOnClickListener {
 
@@ -23,7 +30,11 @@ class MainActivity : AppCompatActivity() {
 
         //open new fragment with customization options
         binding.customizeButton.setOnClickListener {
-
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, CustomizationFragment())
+                .addToBackStack(null)
+                .commit()
         }
 
         //manual on how to use the app
