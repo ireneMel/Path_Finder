03a3c45c52edaea4b11772d5163f8546e6e2d69c
ireneMiller <iramelnykv@gmail.com>
2022-06-23 09:43:47 +0300

    added theme manager to set theme

---
+++ app/src/main/java/com/example/pathfinder/customization/ThemeManager.kt
@@ -0,0 +1,19 @@
+package com.example.pathfinder.customization
+
+import android.content.Context
+import com.example.pathfinder.R
+
+class ThemeManager {
+    companion object {
+        fun setCustomTheme(context: Context, theme: String) {
+            when (theme) {
+                ThemeNames.BASIC -> context.setTheme(R.style.Theme_PathFinder)
+                ThemeNames.SPRING -> context.setTheme(R.style.Spring)
+                ThemeNames.DRACULA -> context.setTheme(R.style.Dracula)
+                ThemeNames.SEA -> context.setTheme(R.style.Sea)
+                ThemeNames.GRAPE -> context.setTheme(R.style.Grape)
+                else -> context.setTheme(R.style.Theme_PathFinder)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/pathfinder/customization/ThemeNames.kt
@@ -0,0 +1,11 @@
+package com.example.pathfinder.customization
+
+class ThemeNames {
+    companion object {
+        const val BASIC = "Basic"
+        const val DRACULA = "Dracula"
+        const val GRAPE = "Grape"
+        const val SEA = "Sea"
+        const val SPRING = "Spring"
+    }
+}
