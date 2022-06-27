3549485e357acc03c9be300645ae66adaee31c41
ireneMiller <iramelnykv@gmail.com>
2022-06-27 11:52:16 +0300

    deleted commented code

--- app/src/main/java/com/example/pathfinder/models/ThemeModel.kt
+++ app/src/main/java/com/example/pathfinder/models/ThemeModel.kt
@@ -1,10 +1,5 @@
 package com.example.pathfinder.models
 
-import android.content.res.Resources
-import android.graphics.Color
-import android.widget.ImageView
-import android.widget.TextView
-
 data class ThemeModel(
     val themeName: String,
 //    val image: ImageView //preview of graph?
