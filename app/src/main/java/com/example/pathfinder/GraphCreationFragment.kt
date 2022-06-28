ae2ba937491e212fa5c34e4e050a1399a0d258d8
ireneMiller <iramelnykv@gmail.com>
2022-06-28 13:36:12 +0300

    added ability to load file from main fragment

--- app/src/main/java/com/example/pathfinder/GraphCreationFragment.kt
+++ app/src/main/java/com/example/pathfinder/GraphCreationFragment.kt
@@ -18,101 +18,115 @@ import com.example.pathfinder.core.serialization.write.SaveGraphToFile
 import com.example.pathfinder.databinding.FragmentGraphCreationBinding
 import kotlinx.coroutines.flow.collect
 
-@Suppress("UNREACHABLE_CODE")
 class GraphCreationFragment : Fragment(R.layout.fragment_graph_creation) {
-	private lateinit var binding: FragmentGraphCreationBinding
-	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-		super.onViewCreated(view, savedInstanceState)
-		
-		binding = FragmentGraphCreationBinding.bind(view)
-		val uiGraph = UIGraph(25f, Paint().apply {
-			color = Color.CYAN
-		}, Paint().apply {
-			color = Color.GRAY
-			strokeWidth = 3f
-		}, Paint().apply {
-			color = Color.GRAY
-			strokeWidth = 3f
-		}, Paint().apply {
-			color = Color.GRAY
-			textSize = 18f
-		}, 4f
-		)
-		val vertexFinder = FindUIVertex(60f)
-		val edgeFinder = FindUIEdge(45f)
-		var mode = 0
-		val modes = listOf(
-			AddVertexMode,
-			AddEdgeMode(vertexFinder),
-			RemoveVertexMode(vertexFinder, edgeFinder),
-			SetPriceMode(vertexFinder, edgeFinder)
-		)
-		val texts = listOf("Add vertex", "Add edge", "Remove", "Set price")
-		binding.graph.touchMode = modes[mode]
-		binding.graph.graph = uiGraph
-		binding.buttonChange.text = texts[mode]
-		
-		binding.buttonChange.setOnClickListener {
-			mode = (mode + 1) % modes.size
-			binding.graph.touchMode = modes[mode]
-			binding.buttonChange.text = texts[mode]
-			if (modes[mode] is DrawMode) {
-				binding.graph.drawMode = modes[mode] as DrawMode
-			}
-		}
-		lifecycleScope.launchWhenStarted {
-			graphReader.state.collect {
-				when (it) {
-					ReadState.ERROR  -> makeToast("Error while reading file.")
-					is ReadState.FINISHED -> {
-						uiGraph.setGraph(it.graph)
-						makeToast("Success")
-					}
-					else             -> {}
-				}
-			}
-		}
-		binding.open.setOnClickListener {
-			graphReader.openFile()
-		}
-		
-		
-		lifecycleScope.launchWhenStarted {
-			graphSaver.state.collect {
-				when (it) {
-					FileState.ERROR  -> makeToast("Error while saving file.")
-					FileState.CLOSED -> makeToast("Success")
-					else             -> {}
-				}
-			}
-		}
-		binding.save.setOnClickListener {
-			val graph = binding.graph.graph
-			if (graph != null) {
-				graphSaver.createFile(graph)
-			} else {
-				makeToast("The canvas must not be empty")
-			}
-		}
-	}
-	
-	private val graphSaver: SaveGraphToFile by lazy {
-		SaveGraphToFile(
-			requireActivity().activityResultRegistry,
-			viewLifecycleOwner,
-			requireActivity().contentResolver
-		)
-	}
-	
-	private val graphReader: ReadGraphFromFile by lazy {
-		ReadGraphFromFile(
-			requireActivity().activityResultRegistry,
-			viewLifecycleOwner,
-			requireActivity().contentResolver
-		)
-	}
-	
-	private fun makeToast(text: String) =
-		Toast.makeText(requireContext(), text, Toast.LENGTH_SHORT).show()
-	
+    private lateinit var binding: FragmentGraphCreationBinding
+    private lateinit var uiGraph: UIGraph
+
+    companion object {
+        var OPEN_CLICKED = false
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentGraphCreationBinding.bind(view)
+        uiGraph = UIGraph(25f, Paint().apply {
+            color = Color.CYAN
+        }, Paint().apply {
+            color = Color.GRAY
+            strokeWidth = 3f
+        }, Paint().apply {
+            color = Color.GRAY
+            strokeWidth = 3f
+        }, Paint().apply {
+            color = Color.GRAY
+            textSize = 18f
+        }, 4f
+        )
+        val vertexFinder = FindUIVertex(60f)
+        val edgeFinder = FindUIEdge(45f)
+        var mode = 0
+        val modes = listOf(
+            AddVertexMode,
+            AddEdgeMode(vertexFinder),
+            RemoveVertexMode(vertexFinder, edgeFinder),
+            SetPriceMode(vertexFinder, edgeFinder)
+        )
+        val texts = listOf("Add vertex", "Add edge", "Remove", "Set price")
+        binding.graph.touchMode = modes[mode]
+        binding.graph.graph = uiGraph
+        binding.buttonChange.text = texts[mode]
+
+        //load graph when corresponding button was licked
+        if (OPEN_CLICKED) {
+            openClicked()
+            OPEN_CLICKED = false
+        }
+
+        binding.open.setOnClickListener { openClicked() }
+
+        binding.buttonChange.setOnClickListener {
+            mode = (mode + 1) % modes.size
+            binding.graph.touchMode = modes[mode]
+            binding.buttonChange.text = texts[mode]
+            if (modes[mode] is DrawMode) {
+                binding.graph.drawMode = modes[mode] as DrawMode
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            graphSaver.state.collect {
+                when (it) {
+                    FileState.ERROR -> makeToast("Error while saving file.")
+                    FileState.CLOSED -> makeToast("Success")
+                    else -> {}
+                }
+            }
+        }
+        binding.save.setOnClickListener {
+            val graph = binding.graph.graph
+            if (graph != null) {
+                graphSaver.createFile(graph)
+            } else {
+                makeToast("The canvas must not be empty")
+            }
+        }
+    }
+
+    private fun openClicked() {
+        lifecycleScope.launchWhenStarted {
+            graphReader.state.collect {
+                when (it) {
+                    ReadState.ERROR -> makeToast("Error while reading file.")
+                    is ReadState.FINISHED -> {
+                        uiGraph.setGraph(it.graph)
+                        makeToast("Success")
+                    }
+                    else -> {}
+                }
+            }
+        }
+
+        graphReader.openFile()
+    }
+
+    private val graphSaver: SaveGraphToFile by lazy {
+        SaveGraphToFile(
+            requireActivity().activityResultRegistry,
+            viewLifecycleOwner,
+            requireActivity().contentResolver
+        )
+    }
+
+    private val graphReader: ReadGraphFromFile by lazy {
+        ReadGraphFromFile(
+            requireActivity().activityResultRegistry,
+            viewLifecycleOwner,
+            requireActivity().contentResolver
+        )
+    }
+
+    private fun makeToast(text: String) =
+        Toast.makeText(requireContext(), text, Toast.LENGTH_SHORT).show()
+
 }
