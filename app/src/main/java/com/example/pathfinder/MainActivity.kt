e7dba98ad16164b6a55a653491f2cf014e0fc131
ireneMiller <iramelnykv@gmail.com>
2022-06-28 10:27:52 +0300

    added call to MainFragment from MainActivity

--- app/src/main/java/com/example/pathfinder/MainActivity.kt
+++ app/src/main/java/com/example/pathfinder/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.pathfinder
 
 import android.os.Bundle
+import android.text.TextUtils.replace
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.fragment.app.commit
@@ -15,9 +16,11 @@ class MainActivity : AppCompatActivity() {
 			this,
 			ThemeStorage.getThemeColor(this).toString()
 		)
+
 		if (savedInstanceState == null) {
 			supportFragmentManager.commit {
-				replace(R.id.container, GraphCreationFragment())
+				replace(R.id.container, MainFragment())
+//				replace(R.id.container, GraphCreationFragment())
 			}
 		}
 	}
