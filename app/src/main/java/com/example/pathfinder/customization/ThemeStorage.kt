f5b78da8d899e11ce61643a04f40966677fa8f3f
ireneMiller <iramelnykv@gmail.com>
2022-07-04 07:43:26 +0300

    migrated to object

--- app/src/main/java/com/example/pathfinder/customization/ThemeStorage.kt
+++ app/src/main/java/com/example/pathfinder/customization/ThemeStorage.kt
@@ -3,24 +3,21 @@ package com.example.pathfinder.customization
 import android.content.Context
 import android.content.SharedPreferences
 
-class ThemeStorage {
+object ThemeStorage {
+    private const val sharedPreferencesName = "theme_data"
+    private const val key = "theme"
 
-    companion object {
-        private const val sharedPreferencesName = "theme_data"
-        private const val key = "theme"
+    fun saveThemeColor(context: Context, themeName: String?) {
+        val sharedPreferences: SharedPreferences =
+            context.getSharedPreferences(sharedPreferencesName, Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString(key, themeName)
+        editor.apply()
+    }
 
-        fun saveThemeColor(context: Context, themeName: String?) {
-            val sharedPreferences: SharedPreferences =
-                context.getSharedPreferences(sharedPreferencesName, Context.MODE_PRIVATE)
-            val editor = sharedPreferences.edit()
-            editor.putString(key, themeName)
-            editor.apply()
-        }
-
-        fun getThemeColor(context: Context): String? {
-            val sharedPreferences: SharedPreferences =
-                context.getSharedPreferences(sharedPreferencesName, Context.MODE_PRIVATE)
-            return sharedPreferences.getString(key, "Basic")
-        }
+    fun getThemeColor(context: Context): String? {
+        val sharedPreferences: SharedPreferences =
+            context.getSharedPreferences(sharedPreferencesName, Context.MODE_PRIVATE)
+        return sharedPreferences.getString(key, "Basic")
     }
 }
